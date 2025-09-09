93e7e71770097e543f1fa51d42db8950e6218bbe
Giri Chandana <girichandana2758@gmail.com>
2025-09-09 20:26:28 +0530

    variables.go pushed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golanglearning
+
+go 1.25.1
---
+++ variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func PrintVariables() {
+	var a int = 5
+	fmt.Println("int a = ", a)
+	var b string = "Hello world using a variable"
+	fmt.Println("string b = ", b)
+
+}
