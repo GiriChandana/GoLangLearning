98f943b83a18b8fdd0a62026a04f32c00d8b77d4
Giri Chandana <girichandana2758@gmail.com>
2025-09-10 10:58:42 +0530

    Modified variables.go file

--- variables.go
+++ variables.go
@@ -7,5 +7,9 @@ func PrintVariables() {
 	fmt.Println("int a = ", a)
 	var b string = "Hello world using a variable"
 	fmt.Println("string b = ", b)
+	var c bool = false
+	fmt.Println("boolean c =", c)
+	var d float32 = 5
+	fmt.Println("float32 d = ", d)
 
 }
