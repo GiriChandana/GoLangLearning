b4803d8bec3e415ea2eefb1dbe5115161c7ef077
Giri Chandana <girichandana2758@gmail.com>
2025-09-10 11:41:27 +0530

    Global variable declaration

--- VariableScope.go
+++ VariableScope.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 
+var course string = "Kodecloud"
+
 func VariableScope() {
 	country := "India"
 	{
 		city := "Hyderabad"
 		fmt.Printf("Country = %v, City = %v ", country, city)
 		fmt.Println()
+		fmt.Printf("Global Variable inside block = %v", course)
+		fmt.Println()
 	}
 	//fmt.Printf("Country = %v, City = %v ", country, city)
+	fmt.Printf("Global Variable outside block = %v", course)
 }
