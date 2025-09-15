9e70ae56675759831610f555f4144ae3e1c07e09
Giri Chandana <girichandana2758@gmail.com>
2025-09-15 11:48:10 +0530

    decrypt

--- cryptit/main.go
+++ cryptit/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/GiriChandana/cryptit/decrypt"
 	"github.com/GiriChandana/cryptit/encrypt"
 )
 
 func main() {
 	fmt.Println(encrypt.Nimbus("KodeCloud"))
+	encryptedStr := encrypt.Nimbus("KodeCloud")
+	fmt.Println("Decrypted string: ", decrypt.Nimbus(encryptedStr))
 }
