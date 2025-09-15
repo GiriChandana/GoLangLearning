6679714de66a83b51adda18378d67305ab6c9f69
Giri Chandana <girichandana2758@gmail.com>
2025-09-15 10:58:38 +0530

    encrypt module

---
+++ cryptit/go.mod
@@ -0,0 +1,3 @@
+module github.com/GiriChandana/cryptit
+
+go 1.25.1
---
+++ cryptit/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GiriChandana/cryptit/encrypt"
+)
+
+func main() {
+	fmt.Println(encrypt.Nimbus("KodeCloud"))
+}
